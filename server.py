94698f9c1a233a598505db7815c9c43705ada2b6
Evi14 <etlevaminga@gmail.com>
2023-01-10 09:53:11 +0100

    new

--- server.py
+++ server.py
@@ -1,9 +1,5 @@
 from flask_app import app
-# importo modelet dhe kontrollet
-from flask_app.models.user import User
 from flask_app.controllers import users
-# from flask_app.models.ninja import Ninja
-# from flask_app.controllers import ninjas
 
 if __name__ == "__main__":
     app.run(debug=True)
